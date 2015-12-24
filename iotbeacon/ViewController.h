fa82a9a5bdff6065c68de49055019784c695f1dc
vtyutin <Vladimir.Tyutin@symphonyteleca.com>
2015-12-24 15:23:19 +0300

    Initial Commit

---
+++ iotbeacon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iotbeacon
+//
+//  Created by Vladimir on 24/12/15.
+//  Copyright © 2015 BIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iotbeacon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iotbeacon
+//
+//  Created by Vladimir on 24/12/15.
+//  Copyright © 2015 BIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
